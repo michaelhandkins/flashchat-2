fb30405c314f488725e31c11f9fba4312862283c
Michael Handkins <michaelhandkins@gmail.com>
2020-09-14 15:10:44 -0500

    Create table view data source methods for chat view controller

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,10 +1,5 @@
-//
 //  ChatViewController.swift
 //  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
 
 import UIKit
 import Firebase
@@ -14,9 +9,12 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [Message(sender: "1@2.com", body: "hey"), Message(sender: "a@b.com", body: "yo"), Message(sender: "1@2.com", body: "exactly..")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
         
@@ -35,6 +33,24 @@ class ChatViewController: UIViewController {
             print ("Error signing out: %@", signOutError)
         }
           
+        
+    }
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        
+        cell.textLabel?.text = messages[indexPath.row].body
+        
+        return cell
         
     }
     
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Michael Handkins on 9/14/20.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender: String //email address of sender
+    let body: String //the actual message
+    
+}
