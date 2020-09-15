ee3fa575b9dbd71f930c0617bcc59ca26fd6b6b2
Michael Handkins <michaelhandkins@gmail.com>
2020-09-15 11:19:12 -0500

    Sort messages by date and change firestore rules so that only authenticated users can read and write data

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -32,7 +32,7 @@ class ChatViewController: UIViewController {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField : messageSender,
                 K.FStore.bodyField : messageBody,
-                K.FStore.dateField : Date()]) { (error) in
+                K.FStore.dateField : Date().timeIntervalSince1970]) { (error) in
                     if let e = error {
                         print("There was an issue saving data to Firestore: \(e)")
                     } else {
@@ -59,7 +59,7 @@ class ChatViewController: UIViewController {
     
     func loadMessages() {
         
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener { (querySnapshot, error) in
             self.messages = []
             if let e = error {
                 print("There was an issue retrieving data from Firestore: \(e)")
