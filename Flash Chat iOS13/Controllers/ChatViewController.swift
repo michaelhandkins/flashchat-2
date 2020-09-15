cc9fb58b8df9ee6ae8c40991739e8066a2f1a67c
Michael Handkins <michaelhandkins@gmail.com>
2020-09-15 10:34:38 -0500

    Add functionality to store sent messages in Firestore and then retrieve and display them when chat view loads

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -9,12 +9,16 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var messages: [Message] = [Message(sender: "1@2.com", body: "hey"), Message(sender: "a@b.com", body: "yo"), Message(sender: "1@2.com", body: "exactly..")]
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
+        
         tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
@@ -22,6 +26,22 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField : messageSender,
+                K.FStore.bodyField : messageBody,
+                K.FStore.dateField : Date()]) { (error) in
+                    if let e = error {
+                        print("There was an issue saving data to Firestore: \(e)")
+                    } else {
+                        self.loadMessages()
+                    }
+            }
+            
+        }
+        
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -36,6 +56,32 @@ class ChatViewController: UIViewController {
           
         
     }
+    
+    func loadMessages() {
+        
+        messages = []
+        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+            if let e = error {
+                print("There was an issue retrieving data from Firestore: \(e)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
     
 }
 
